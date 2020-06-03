8b348c3d2f889188f863662b2e6ecb8aed215813
ashok997 <ashok997@gmail.com>
2020-06-03 15:14:58 -0500

    added bio and projects components wireframe

---
+++ src/Projects.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Projects</h1>
+        <p> Some description</p>
+        <li> Project 1</li>
+        <li> Project 2</li>
+        <li> Project 3</li>
+      </div>
+    );
+  }
+}
