8e47c6afe69a2f0cc4800895cbe80aca76bb0742
ashok997 <ashok997@gmail.com>
2020-06-13 17:27:07 -0500

    added more images and rearranged file structure

--- src/TechnicalSkills.js
+++ src/TechnicalSkills.js
@@ -9,36 +9,82 @@ export default class TechnicalSkills extends Component {
         <Container>
           <Figure>
             <Figure.Image
-              src="/javascript.svg"
+              src="/images/javascript.svg"
               width="100"
               height="100"
               rounded
             />
             <Figure.Caption>JavaScript</Figure.Caption>
-            <Figure.Image src="/ruby.png" width="100" height="100" rounded />
+            <Figure.Image
+              src="/images/ruby.png"
+              width="100"
+              height="100"
+              rounded
+            />
             <Figure.Caption>Ruby</Figure.Caption>
-            <Figure.Image src="/java.png" width="100" height="100" rounded />
+            <Figure.Image
+              src="/images/java.png"
+              width="100"
+              height="100"
+              rounded
+            />
             <Figure.Caption>Java</Figure.Caption>
-            <Figure.Image src="/rails.png" width="100" height="100" rounded />
+            <Figure.Image
+              src="/images/rails.png"
+              width="100"
+              height="100"
+              rounded
+            />
             <Figure.Caption>Ruby on Rails</Figure.Caption>
-            <Figure.Image src="/logo512.png" width="100" height="100" rounded />
+            <Figure.Image
+              src="/images/logo512.png"
+              width="100"
+              height="100"
+              rounded
+            />
             <Figure.Caption>React</Figure.Caption>
-            <Figure.Image src="/html.png" width="100" height="100" rounded />
+            <Figure.Image
+              src="/images/redux.png"
+              width="100"
+              height="100"
+              rounded
+            />
+            <Figure.Caption center>Redux</Figure.Caption>
+            <Figure.Image
+              src="/images/html.png"
+              width="100"
+              height="100"
+              rounded
+            />
             <Figure.Caption center>HTML</Figure.Caption>
-            <Figure.Image src="/css.png" width="100" height="100" rounded />
+            <Figure.Image
+              src="/images/css.png"
+              width="100"
+              height="100"
+              rounded
+            />
             <Figure.Caption center>CSS</Figure.Caption>
             <Figure.Image
-              src="/bootstrap.png"
+              src="/images/bootstrap.png"
               width="100"
               height="100"
               rounded
             />
             <Figure.Caption>Bootstrap</Figure.Caption>
-            <Figure.Image src="/git.png" width="100" height="100" rounded />
+            <Figure.Image
+              src="/images/git.png"
+              width="100"
+              height="100"
+              rounded
+            />
             <Figure.Caption center>Git</Figure.Caption>
-            <li>Redux</li>
-
-            <li>SQL</li>
+            <Figure.Image
+              src="/images/sql.jpeg"
+              width="100"
+              height="100"
+              rounded
+            />
+            <Figure.Caption center>SQL</Figure.Caption>
           </Figure>
         </Container>
       </div>
