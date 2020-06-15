29fb9e96c848042f7df42bdedfed5a98fb1c3ac4
ashok997 <ashok997@gmail.com>
2020-06-15 13:44:39 -0500

    arranged icons in rows and columns

--- src/TechnicalSkills.js
+++ src/TechnicalSkills.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Figure from "react-bootstrap/Figure";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default class TechnicalSkills extends Component {
   render() {
@@ -8,83 +10,129 @@ export default class TechnicalSkills extends Component {
       <div>
         <Container>
           <Figure>
-            <Figure.Image
-              src="/images/javascript.svg"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption>JavaScript</Figure.Caption>
-            <Figure.Image
-              src="/images/ruby.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption>Ruby</Figure.Caption>
-            <Figure.Image
-              src="/images/java.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption>Java</Figure.Caption>
-            <Figure.Image
-              src="/images/rails.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption>Ruby on Rails</Figure.Caption>
-            <Figure.Image
-              src="/images/logo512.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption>React</Figure.Caption>
-            <Figure.Image
-              src="/images/redux.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption center>Redux</Figure.Caption>
-            <Figure.Image
-              src="/images/html.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption center>HTML</Figure.Caption>
-            <Figure.Image
-              src="/images/css.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption center>CSS</Figure.Caption>
-            <Figure.Image
-              src="/images/bootstrap.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption>Bootstrap</Figure.Caption>
-            <Figure.Image
-              src="/images/git.png"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption center>Git</Figure.Caption>
-            <Figure.Image
-              src="/images/sql.jpeg"
-              width="100"
-              height="100"
-              rounded
-            />
-            <Figure.Caption center>SQL</Figure.Caption>
+            <Row className="justify-content-md-center">
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/javascript.svg"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  JavaScript
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/ruby.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  Ruby
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/java.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  Java
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/rails.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  Ruby on Rails
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/logo512.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  React
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/redux.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  Redux
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/html.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  HTML
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/css.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  CSS
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/bootstrap.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  Bootstrap
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/git.png"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  Git
+                </Figure.Caption>
+              </Col>
+              <Col md="auto">
+                <Figure.Image
+                  src="/images/sql.jpeg"
+                  width="100"
+                  height="100"
+                  rounded
+                />
+                <Figure.Caption class="figure-caption text-center">
+                  SQL
+                </Figure.Caption>
+              </Col>
+            </Row>
           </Figure>
         </Container>
       </div>
