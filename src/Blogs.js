9431c0521a5c0d10ec3d2d6be1bbcdec537cdce9
ashok997 <ashok997@gmail.com>
2020-06-25 14:51:21 -0500

    links to the blogs are hookedup

--- src/Blogs.js
+++ src/Blogs.js
@@ -26,39 +26,19 @@ export default class Blogs extends Component {
   render() {
     return (
       <div style={{ background: "papayawhip" }}>
-        <Container style={{ paddingTop: 50, paddingBottom: 50 }}>
+        <Container
+          style={{ paddingTop: 50, paddingBottom: 50, fontFamily: "Verdana" }}
+        >
           <Alert variant="dark">
             <Alert.Heading>Blogs</Alert.Heading>
           </Alert>
           {this.state.blogs.map((blog) => (
-            <p>{blog.title}</p>
+            <ListGroup as="ul">
+              <ListGroup.Item action href={blog.link}>
+                <p>{blog.title}</p>
+              </ListGroup.Item>
+            </ListGroup>
           ))}
-          <ListGroup as="ul">
-            <ListGroup.Item
-              action
-              href="https://medium.com/@ashok997/intro-to-javascript-for-sportsfans-76ba53ec31c8"
-            >
-              Blog 1
-            </ListGroup.Item>
-            <ListGroup.Item
-              action
-              href="https://medium.com/@ashok997/intro-to-javascript-for-sportsfans-2-7ebc2dfdd6c7"
-            >
-              Blog 2
-            </ListGroup.Item>
-            <ListGroup.Item
-              action
-              href="https://medium.com/@ashok997/intro-to-javascript-for-sportsfans-3-d58c7355f00e"
-            >
-              Blog 3
-            </ListGroup.Item>
-            <ListGroup.Item
-              action
-              href="https://medium.com/@ashok997/intro-to-javascript-for-sportsfans-4-4b2ebf7b5754"
-            >
-              Blog 4
-            </ListGroup.Item>
-          </ListGroup>
         </Container>
       </div>
     );
