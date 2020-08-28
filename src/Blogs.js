156befec8bb41b20c54dac5d1ee9ffd60c763f23
ashok997 <ashok997@gmail.com>
2020-08-28 14:22:52 -0500

    added bootstrap spinner that will be rendered while the blogs are loading

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import Container from "react-bootstrap/Container";
+import Spinner from "react-bootstrap/Spinner";
 import Alert from "react-bootstrap/Alert";
 
 export default class Blogs extends Component {
@@ -8,10 +9,13 @@ export default class Blogs extends Component {
     super();
     this.state = {
       blogs: [],
+      loading: false,
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
+
     fetch(
       " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40ashok997"
     )
@@ -19,6 +23,7 @@ export default class Blogs extends Component {
       .then((result) => {
         this.setState({
           blogs: result.items,
+          loading: false,
         });
       });
   }
@@ -26,22 +31,26 @@ export default class Blogs extends Component {
   render() {
     return (
       <div style={{ background: "papayawhip" }}>
-        <Container
-          style={{ paddingTop: 50, paddingBottom: 50, fontFamily: "Verdana" }}
-        >
-          <Alert variant="dark">
-            <Alert.Heading>Blogs</Alert.Heading>
-          </Alert>
-          {this.state.blogs.map((blog) => (
-            <ListGroup as="ul">
-              <ListGroup.Item action href={blog.link} target="_blank">
-                <>
-                  {blog.title} - published {blog.pubDate.split(" ")[0]}
-                </>
-              </ListGroup.Item>
-            </ListGroup>
-          ))}
-        </Container>
+        {this.state.loading ? (
+          <Spinner animation="border" />
+        ) : (
+          <Container
+            style={{ paddingTop: 50, paddingBottom: 50, fontFamily: "Verdana" }}
+          >
+            <Alert variant="dark">
+              <Alert.Heading>Blogs</Alert.Heading>
+            </Alert>
+            {this.state.blogs.map((blog) => (
+              <ListGroup as="ul">
+                <ListGroup.Item action href={blog.link} target="_blank">
+                  <>
+                    {blog.title} - published {blog.pubDate.split(" ")[0]}
+                  </>
+                </ListGroup.Item>
+              </ListGroup>
+            ))}
+          </Container>
+        )}
       </div>
     );
   }
